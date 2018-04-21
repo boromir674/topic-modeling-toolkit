f557fd98bd109cd217f41ce5895084857c96571c
Konstantinos <k.lampridis@hotmail.com>
2018-04-22 00:25:21 +0200

    TODO: support 'auto-piping' according to component type to solve the numerical indexing obvious problems with pipeline settings being turned on/off

--- transform.py
+++ transform.py
@@ -63,7 +63,7 @@ class PipeHandler(object):
             gen = a_pipe.partial_pipe(gen, 5, 7)
             # print [_ for _ in gen]
             us  = [_ for _ in gen]
-            print us
+            # print us
             # dct.add_documents([[_ for _ in gen]])
             self.dct.add_documents([us])
             # print [_ for _ in gen][:10]
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     ph = PipeHandler()
     pipe = ph.create_pipeline(args.config)
     print pipe
-    ph.pipe_files(pipe, nb_sample=10)
+    ph.pipe_files(pipe, nb_sample=60)
 
     # pipeline_settings = configfile2dict(os.path.join(root_dir, 'code', args.config), 'preprocessing')
     # for k, v in pipeline_settings.items():
