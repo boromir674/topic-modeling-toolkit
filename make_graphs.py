055a81191f58cd997cebae6bb205a6a9ecb676e6
Konstantinos <k.lampridis@hotmail.com>
2018-06-09 01:23:19 +0200

    WIP: TODO satisfy 'make_graphs.py' script interface

--- make_graphs.py
+++ make_graphs.py
@@ -48,6 +48,15 @@ def load_results(path_file):
 class GraphMaker(object):
 
     def __init__(self, plot_dir):
+        self.line_designs = ['b-o',
+                             'r--s',
+                             'g-D',
+                             'y-+',
+                             'p-:',
+                             'o-.',
+                             ]
+        # linestyle / ls: Plot linestyle['-', '--', '-.', ':', 'None', ' ', '']
+        # marker: '+', 'o', '*', 's', 'D', ',', '.', '<', '>', '^', '1', '2'
         self.plot = {
             'perplexity': lambda x: create_perplexity_graph(x),
             'all': lambda x: create_graphs(x)
@@ -70,13 +79,30 @@ class GraphMaker(object):
             print('Created \'{}\' directory'.format(self.gr_dir))
 
     def save_plot(self, graph_type, results):
+        """
+        Creates plots of various metrics and saves png files
+        :param graph_type:
+        :param results:
+        :return:
+        """
         assert graph_type in self.plot or graph_type == 'all'
         if graph_type == 'all':
             graphs = create_graphs(results)
             for gr in graphs:
-                self._save_plot(gr[1], gr[0])
+                self._save_plot(gr[0], gr[1])
         else:
             plot = self.plot[graph_type](results)
+            self._save_plot(graph_type, plot)
+
+    def save_cross_models_plots(self, results_list):
+        """
+        Currently supported maximum 6 plots on the same figure\n.
+        :param results_list:
+        :return:
+        """
+        assert len(results_list) <= len(self.line_designs)
+        graphs = create_cross_graphs(results_list, self.line_designs, [res['model_label'] for res in results_list])
+        for graph_type, plot in graphs:
             self._save_plot(graph_type, plot)
 
     def _iter_prepend(self, int_num):
@@ -99,6 +125,11 @@ class GraphMaker(object):
         print('Saved figure as', target_name)
         self._inds[graph_type] += 1
 
+    # Advanced grid modification
+    # eplot.new_plot(x, 1 / (1 + x), '-s', label=r"$y = \frac{1}{1+x}$", c='#fdb462')
+    # eplot.grid(which='major', axis='x', linewidth=2, linestyle='--', color='b', alpha=0.5)
+    # eplot.grid(which='major', axis='y', linewidth=2, linestyle='-', color='0.85', alpha=0.5)
+
 
 #     eplot = EasyPlot(x, results['trackables'], 'b-o', label='y1 != x**2', showlegend=True, xlabel='x', ylabel='y', title='title', grid='on')
 #     eplot.iter_plot(x, y_dict, linestyle=linestyle_dict, marker=marker_dict, label=labels_dict, linewidth=3, ms=10, showlegend=True, grid='on')
@@ -117,16 +148,35 @@ def create_graphs(results):
         for sub_score, values in sub_score2values.items():
             sub_plot_name = eval_name+'-'+sub_score
             try:
-                grs.append((EasyPlot(range(len(values)), values, 'b-o', label=sub_plot_name, showlegend=True, xlabel='x', ylabel='y', title='title', grid='on'),
-                            sub_plot_name))
+                grs.append((
+                    sub_plot_name,
+                    EasyPlot(range(len(values)), values, 'b-o', label=sub_plot_name, showlegend=True, xlabel='x', ylabel='y', title='title', grid='on')))
             except TypeError as e:
                 print('Failed to create {} plot: type({}) = {}'.format(sub_plot_name, sub_score, type(values)))
     return grs
 
 
-def make_plots():
-    # assert all(map(lambda x: len(x) == sum(self.collection_passes), [score_list for scores_dict in self.trackables.values() for score_list in scores_dict]))
+def create_cross_graphs(results_list, line_design_list, labels_list):
+    assert len(results_list) == len(line_design_list) == len(labels_list)
+    assert len(set([res['root_dir'] for res in results_list])) <= 1
+    models = '-'.join(labels_list)
+    graph_plots = []
+    for eval_name, sub_score2values in results_list[0]['trackables'].items():
+        for sub_score, values in sub_score2values.items():
+            measure_name = eval_name + '-' + sub_score
+            try:
+                x = range(len(values))
+                graph_plots.append((
+                    '{}-{}'.format(models, measure_name),
+                    EasyPlot(x, values, line_design_list[0], label=labels_list[0], showlegend=True, xlabel='x', ylabel='y', title=results_list[0]['root_dir'], grid='on')))
+                for j, result in enumerate(results_list[1:]):
+                    graph_plots[-1].add_plot(x, result['trackables'][eval_name][sub_score], line_design_list[j+1], label=labels_list[j+1])
+            except TypeError as e:
+                print('Failed to create {} plot: type({}) = {}'.format(measure_name, sub_score, type(values)))
+    return grs
 
+
+def make_plots():
     x = [1, 2, 3, 4, 5]
     y1 = [1, 4, 7, 9, 4]
     y2 = [2, 3, 5, 8, 9]
@@ -159,11 +209,12 @@ def _get_results_file_path(collection_name):
 
 if __name__ == '__main__':
     args = get_cl_arguments()
-    results = load_results(_get_results_file_path(args.collection))
+    exp_results = load_results(_get_results_file_path(args.collection))
 
     plotter = GraphMaker(os.path.join(collections_dir, args.collection, 'graphs'))
-    plotter.save_plot('all', results)
+    # plotter.save_plot('all', exp_results)
 
+    plotter.save_cross_models_plots()
 
 # eplot = EasyPlot(xlabel=r'$x$', ylabel='$y$', fontsize=16,
 #                  colorcycle=["#66c2a5", "#fc8d62", "#8da0cb"], figsize=(8, 5))
