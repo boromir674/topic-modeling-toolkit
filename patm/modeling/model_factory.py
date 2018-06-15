65299bd4bd826b614d2952a43cbb10af542b471f
Konstantinos <k.lampridis@hotmail.com>
2018-06-15 20:15:54 +0200

    Implement creating TopicModel and TrainSpecs from a dumped phi matrix file and a dictionry holding experimental results

--- patm/modeling/model_factory.py
+++ patm/modeling/model_factory.py
@@ -2,7 +2,8 @@ import artm
 from .topic_model import TopicModel, TrainSpecs
 from ..evaluation.scorer_factory import get_scorers_factory
 from patm.utils import cfg2model_settings
-from .regularizers import init_from_file
+from .regularizers import init_from_file, init_from_latest_results
+from patm.definitions import collections_dir
 
 
 dicts2model_factory = {}
@@ -12,6 +13,12 @@ def get_model_factory(dictionary):
     if dictionary not in dicts2model_factory:
         dicts2model_factory[dictionary] = ModelFactory(dictionary)
     return dicts2model_factory[dictionary]
+
+
+# def create_model_factory(bin_dict_path):
+#     dictionry = artm.Dictionary()
+#     dictionry.load(bin_dict_path)
+#     return dictionry
 
 
 class ModelFactory(object):
@@ -29,10 +36,11 @@ class ModelFactory(object):
             'topic-kernel': lambda x: artm.TopicKernelScore(name=x, probability_mass_threshold=0.3),
             'top-tokens': lambda x: artm.TopTokensScore(name=x)
         }
+    self._tm = None
 
-    def create_model(self, label, cfg_file, output_dir):
+    def create_model(self, label, cfg_file, reg_cfg):
         """
-        Creates an artm _topic_model instance based on the input config file.\n
+        Creates an artm _topic_model instance based on the input config file. Phi matrix is initialized with random numbers by default\n
         :param str label: the unique identifier for the model
         :param str cfg_file: A cfg file containing initial model parameters, regularizers and score metrics to use
         :param str output_dir: the top level directory to use for dumping files related to training
@@ -50,17 +58,37 @@ class ModelFactory(object):
             model.scores.add(self.score2constructor[score_setting_name](value))
             scorers[score_setting_name] = get_scorers_factory(settings['scores']).create_scorer(value)
 
-        tm = TopicModel(label, model, scorers)
+        self._tm = TopicModel(label, model, scorers)
+        self._set_regularizers(regularizers)
+        specs = TrainSpecs(collection_passes=settings['learning']['collection_passes'])
+        return self._tm, specs
 
-        for reg_instance in regularizers:
-            tm.add_regularizer(reg_instance)
+    def create_model_with_phi_from_disk(self, phi_file_path, results):
+        """
 
-        print 'REGULARIZERS:', ', '.join(x for x in model.regularizers.data)
+        :param phi_file_path:
+        :param results:
+        :return:
+        """
+        regularizers = init_from_latest_results(results)
+        scorers = {}
 
-        specs = TrainSpecs(collection_passes=settings['learning']['collection_passes'], output_dir=output_dir)
+        model = artm.ARTM(dictionary=self.dict)
+        model.load(filename=phi_file_path)
+        model.num_document_passes = results['model_parameters']['document_passes'][-1]
 
-        return tm, specs
+        for score_setting_name, eval_instance_name in results['evaluators']:
+            model.scores.add(self.score2constructor[score_setting_name](eval_instance_name))
+            scorers[score_setting_name] = get_scorers_factory(dict(results['evaluators'])).create_scorer(eval_instance_name)
 
+        self._tm = TopicModel(label, model, scorers)
+        self._set_regularizers(regularizers)
+        specs = TrainSpecs(collection_passes=results['collection_passes'][-1])
+        return self._tm, specs
+
+    def _set_regularizers(self, reg_list):
+        for reg_instance in reg_list:
+            self._tm.add_regularizer(reg_instance)
 
 if __name__ == '__main__':
     sett = cfg2model_settings('/data/thesis/code/train.cfg')
