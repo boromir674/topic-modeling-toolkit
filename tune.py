f5f1f9e93f4ba268cb5bf6278ce4fe6dd4d3134e
Konstantinos <k.lampridis@hotmail.com>
2018-07-09 14:35:47 +0200

    Fix tau coefficient dynamic trajectory building in Tuner

--- tune.py
+++ tune.py
@@ -29,10 +29,8 @@ class Tuner(object):
     - nb_topics                       eg: [20, 40, 60, 80, 100]\n
     - collection_passes               eg: [100]\n
     - document_passes                 eg: [1, 5, 10, 15]\n
-    - sparse_phi_reg_coef_trajectory    eg: {'deactivation_period_pct': [10, 15, 20], 'start': [-0.1, -0.2, -0.3, -0.4], 'end' = [-1, -2, -3, -4]}
-    - sparse_theta_reg_coef_trajectory  eg: {'deactivation_period_pct': [8, 14, 20], 'start': [-0.2, -0.3, -0.4], 'end' = [-1, -3, -5]}
-
-    # deactivation_period = [10, 20, 30]
+    - sparse_phi_reg_coef_trajectory    eg: {'deactivation_period_pct': [0.1, 0.15, 0.2], 'start': [-0.1, -0.2, -0.3, -0.4], 'end' = [-1, -2, -3, -4]}
+    - sparse_theta_reg_coef_trajectory  eg: {'deactivation_period_pct': [0.8, 0.14, 0.2], 'start': [-0.2, -0.3, -0.4], 'end' = [-1, -3, -5]}
 
     """
     def __init__(self, collection, train_config, regularizers_cfg, static_parameters, explorable_parameters, prefix_label=None, append_explorables='all', append_static=True):
@@ -52,8 +50,18 @@ class Tuner(object):
         self._reg_cfg = regularizers_cfg
         self._static_params_hash = OrderedDict(static_parameters)
         self._expl_params = explorable_parameters
-        self._prefix = prefix_label
+        self._expl_params_start_indices = {} # OrderedDict([(k, 0) for k in self.explorable_parameters])
         self._regex = re.compile('^(\w+)_coef_trajectory$')
+        t = 0
+        for k in self.explorable_parameters:
+            self._expl_params_start_indices[k] = t
+            if self._regex.match(k):
+                t += 3
+            else:
+                t += 1
+        print self._expl_params_start_indices
+        self._prefix = prefix_label
+
         self._max_digits_version = 3
         self._labeling_params = []
 
@@ -85,8 +93,7 @@ class Tuner(object):
 
         self._traj_attr2index = OrderedDict([('deactivation_period_pct', 0), ('start', 1), ('end', 2)])
         ses = self._extract_simple_space()+self._extract_trajectory_space()
-        print 'Search space', ses
-        if len(ses) == 1 and type(ses[0]) == list and type(ses[0][0]) == list:
+        if type(ses[0]) == list and type(ses[0][0]) == list:
             ses = [item for subl in ses for item in subl]
         print 'Search space', ses
         self._parameter_grid_searcher = ParameterGrid(ses)
@@ -134,7 +141,11 @@ class Tuner(object):
             self._tm.add_regularizer(reg_obj, regularizers_factory.get_type(reg_obj))
 
         for tau_traj_parameter_name in map(lambda x: x+'_coef_trajectory', self._tau_traj_to_build):
+            assert type(self._val('collection_passes')) == int
+            print 'Building', tau_traj_parameter_name, 'trajectory', self._val(tau_traj_parameter_name)
+            # print self._expl_params
             trs.append(self._build_traj(self._val(tau_traj_parameter_name), self._val('collection_passes')))
+
             reg_type = re.match('^(sparse_\w+)_reg_coef_trajectory$', tau_traj_parameter_name).group(1).replace('_', '-')
             ns.append(self._tm.regularizer_names[self._tm.regularizer_types.index(reg_type)])
 
@@ -144,16 +155,17 @@ class Tuner(object):
         """Parameter value extractor from currently generated parameter vector and from static parameters"""
         if parameter_name in self._static_params_hash:
             return self._static_params_hash[parameter_name]
-        if parameter_name in self.explorable_parameters and parameter_name not in ('sparse_phi_reg_coef_trajectory', 'sparse_theta_reg_coef_trajectory'):
-            return self.parameter_vector[self.explorable_parameters.index(parameter_name)]
+        if parameter_name in self.explorable_parameters and not self._regex.match(parameter_name): #in ('sparse_phi_reg_coef_trajectory', 'sparse_theta_reg_coef_trajectory'):
+            return self.parameter_vector[self._expl_params_start_indices[parameter_name]]
         if parameter_name in self.explorable_parameters:
-            print 'L', len(self.parameter_vector), self.parameter_vector
-            return {'deactivation_period_pct': self.parameter_vector[self.explorable_parameters.index(parameter_name)+self._traj_attr2index['deactivation_period_pct']],
-                    'start': self.parameter_vector[self.explorable_parameters.index(parameter_name)+self._traj_attr2index['start']],
-                    'end': self.parameter_vector[self.explorable_parameters.index(parameter_name)+self._traj_attr2index['end']]}
+            # print 'VAL', self.parameter_vector, parameter_name, self.explorable_parameters.index(parameter_name),
+
+            return {'deactivation_period_pct': self.parameter_vector[self._expl_params_start_indices[parameter_name]+self._traj_attr2index['deactivation_period_pct']],
+                    'start': self.parameter_vector[self._expl_params_start_indices[parameter_name]+self._traj_attr2index['start']],
+                    'end': self.parameter_vector[self._expl_params_start_indices[parameter_name]+self._traj_attr2index['end']]}
 
     def _build_traj(self, traj_def, total_iters):
-        deactivation_span = int(traj_def['deactivation_period_pct'] * total_iters / 100.0)
+        deactivation_span = int(traj_def['deactivation_period_pct'] * total_iters)
         return trajectory_builder.begin_trajectory('tau').deactivate(deactivation_span).interpolate_to(total_iters - deactivation_span, traj_def['end'], start=traj_def['start']).create()
 
     def _extract_simple_space(self):
@@ -235,8 +247,7 @@ if __name__ == '__main__':
 
     #####################
     # REGULARIZERS POOL
-    traj1 = {'deactivation_period_pct': 10, 'start': -0.1, 'end': -18}
-    tr2 = {'deactivation_period_pct': 10, 'start': -0.2, 'end': -12}
+
     reg_pool_specs = [1, {'sparse-theta': {'name': 'spd_t', 'tau': -0.1}, 'smooth-theta': {'name': 'smb_t', 'tau': 1},
                             'sparse-phi': {'name': 'spd_p', 'tau': -0.1}, 'smooth-phi': {'name': 'smb_p', 'tau': 1},
                             'decorrelator-phi': {'name': 'ddt', 'tau': 1e+5}}]
@@ -244,9 +255,11 @@ if __name__ == '__main__':
                           'smooth-phi': {'name': 'smb_p', 'tau': 1}}
                           ]
     ####### EXPLORABLE TRAJECTORIES
-    tr3 = {'deactivation_period_pct': [10], 'start': [-0.2, -0.4], 'end': [-5]}
-    tr4 = {'deactivation_period_pct': [10], 'start': [-0.1, -0.3, -0.5], 'end': [-1, -3, -5]}
-    tr4 = {'deactivation_period_pct': [10], 'start': [-0.2, -0.5, -0.8], 'end': [-1, -4, -7]}
+    tr0 = {'deactivation_period_pct': 0.2, 'start': -0.4, 'end': -8}
+    tr1 = {'deactivation_period_pct': [0.2], 'start': [-0.2, -0.5, -0.8], 'end': [-1, -4, -7]}
+    tr2 = {'deactivation_period_pct': [0.2], 'start': [-0.1, -0.3, -0.5], 'end': [-1, -3, -5]}
+    tr1 = {'deactivation_period_pct': [0.2], 'start': [-0.2, -0.5], 'end': [-3]}
+    tr2 = {'deactivation_period_pct': [0.2], 'start': [-0.1, -0.7], 'end': [-5]}
     ##############################
 
     # static = [('collection_passes', 100), ('document_passes', 5), ('sparse_theta_reg_coef_trajectory', tr2), ('sparse_phi_reg_coef_trajectory', traj1)]
@@ -255,10 +268,11 @@ if __name__ == '__main__':
     # static = [('collection_passes', 100), ('document_passes', 5)]
 
     tuner = Tuner(args.dataset, args.train_cfg, reg_cfg,
-                  [('collection_passes', 100)],
-                  [('nb_topics', [20, 40, 60, 80, 100]), ('document_passes', [5, 10, 15])],
-                  prefix_label='plsa+smt+smp',
-                  append_explorables='all',
+                  [('collection_passes', 10), ('document_passes', 5), ('nb_topics', 20), ('sparse_theta_reg_coef_trajectory', tr0)],
+                  [('sparse_phi_reg_coef_trajectory', tr2)],
+                  prefix_label='plsa+sss+test1',
+                  append_explorables=[],
                   append_static=True)
-    tuner.tune([1, {'sparse-theta': {'name': 'spd_t', 'tau': -0.1}, 'smooth-theta': {'name': 'smb_t', 'tau': 1},
+
+    tuner.tune([0.2, {'sparse-theta': {'name': 'spd_t', 'tau': -0.1}, 'smooth-theta': {'name': 'smb_t', 'tau': 1},
                             'sparse-phi': {'name': 'spd_p', 'tau': -0.1}, 'smooth-phi': {'name': 'smb_p', 'tau': 1}}])
