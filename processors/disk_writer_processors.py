0aefe2522d5cea6fa67f6eeb4f389a1bd0014e18
Konstantinos <k.lampridis@hotmail.com>
2018-09-24 16:06:06 +0200

    Fix variable name in docstring

--- processors/disk_writer_processors.py
+++ processors/disk_writer_processors.py
@@ -2,8 +2,6 @@ from processor import BaseDiskWriter, BaseDiskWriterWithPrologue
 
 
 class UciFormatWriter(BaseDiskWriterWithPrologue):
-# class UciFormatWriter(BaseDiskWriter):
-
     """
     Injests a doc_vector at a time. For example [('2', 1), ('15', 4), ('18', 5), ('11', 3)]
     """
@@ -33,7 +31,7 @@ def write_uci(file_handler, doc_vector, doc_num):
 def write_vowpal(file_handler, doc_vector, doc_num, class_labels):
     """
     Dumps a doument vector as a single line in the specified target file path in the "Vowpal Wabbit" format.\n
-    :param str fname: path to target file
+    :param str file_handler: a file object
     :param iterable doc_vector: the representation of a document; an iterable of (token, frequency) tuples; eg [('_builder', 1), ('alpha', 4)]
     :param int doc_num: number to represent document number in queue; eg 1,2,3,4,5 ... D
     :param dict class_labels: keys are class "category" (i.e. 'author') and values are the actual class the document belongs to (i.e. 'Ivan Sokolov')
