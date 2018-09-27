b1aa3fc561df4f569d60b5199fc7ef5d46735a8e
Konstantinos <k.lampridis@hotmail.com>
2018-09-27 14:02:33 +0200

    Implement GenericTopicNamesBuilder to delegate all background/domain topic names list creation requests

--- patm/utils.py
+++ patm/utils.py
@@ -20,6 +20,39 @@ def cfg2model_settings(cfg_file):
     config.read(cfg_file)
     return OrderedDict([(section.encode('utf-8'), OrderedDict([(setting_name.encode('utf-8'), _section2encoder[section](value)) for setting_name, value in config.items(section) if value])) for section in config.sections()])
 
+class GenericTopicNamesBuilder:
+    def __init__(self, nb_topics=0, background_topics_pct=0.0):
+        self._nb_topics = nb_topics
+        self._background_pct = background_topics_pct
+        self._background_topics, self._domain_topics = [], []
+
+    def _get_generic_topic_names(self, a_range):
+        return ['top_' + index for index in map(lambda x: str(x) if len(str(x)) > 1 else '0' + str(x), range(*a_range))]
+
+    def define_nb_topics(self, nb_topics):
+        if nb_topics != self._nb_topics:
+            self._background_topics, self._domain_topics = [], []
+            self._nb_topics = nb_topics
+        return self
+    def define_background_pct(self, pct):
+        if pct != self._background_pct:
+            self._background_topics, self._domain_topics = [], []
+            self._background_pct = pct
+        return self
+
+    def get_background_topics(self):
+        if not self._background_topics:
+            self._background_topics = self._get_generic_topic_names([int(self._background_pct * self._nb_topics)])
+        return self._background_topics
+    def get_domain_topics(self):
+        if not self._domain_topics:
+            self._domain_topics = self._get_generic_topic_names([int(self._background_pct * self._nb_topics), self._nb_topics])
+        return self._domain_topics
+    def get_background_n_domain_topics(self):
+        return self.get_background_topics(), self.get_domain_topics()
+
+generic_topic_names_builder = GenericTopicNamesBuilder()
+
 
 class Spinner:
     busy = False
