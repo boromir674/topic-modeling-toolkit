ccf8e27629cb885103667522726d504c3013bdab
Konstantinos <>
2019-07-29 13:02:23 +0200

    Implement an alternative Vowpal disk writer that includes a weight of 1.0 for each datapoint (document in BoW) and first writes information about modalities and then about the DEFAULT modality, '@default_class'

--- patmtk/processors/disk_writer_processors.py
+++ patmtk/processors/disk_writer_processors.py
@@ -43,3 +43,20 @@ def write_vowpal(file_handler, doc_vector, doc_num, class_labels):
         ' '.join(map(lambda x: '|{} {}'.format(x[0], x[1]), class_labels.items()))
     ))
 freq2string = {1: ''}
+
+def write_vowpal_v2(file_handler, doc_vector, doc_num, class_labels):
+    """
+    Dumps a document vector as a single line in the specified target file path in the "Vowpal Wabbit" format.\n
+    :param str file_handler: a file object
+    :param iterable doc_vector: the representation of a document; an iterable of (token, frequency) tuples; eg [('_builder', 1), ('alpha', 4)]
+    :param int doc_num: number to represent document number in queue; eg 1,2,3,4,5 ... D
+    :param dict class_labels: keys are class "category" (i.e. 'author') and values are the actual class the document belongs to (i.e. 'Ivan Sokolov')
+    """
+    file_handler.write('doc{} 1.0 {} |@default_class {}\n'.format(
+        doc_num,
+        ' '.join(map(lambda x: '|{} {}'.format(x[0], ' '.join([str(_) for _ in modality_features[type(x[1])](x[1])])), class_labels.items())),
+        ' '.join(map(lambda x: '{}{}'.format(x[0].encode('utf-8'), freq2string.get(x[1], ':{}'.format(x[1]))), doc_vector)),
+        # ' '.join(map(lambda x: '{}{}'.format(x[0], freq2string.get(x[1], ':{}'.format(x[1]))), doc_vector)),
+    ))
+modality_features = {str: lambda x: [x],
+                     list: lambda x: x}
