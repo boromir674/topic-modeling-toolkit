4cef66196571c2a60dc28326884bb9afc356ddd0
Konstantinos <>
2019-08-05 12:59:14 +0200

    Update setup.py

--- patmtk/setup.py
+++ patmtk/setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='patmtk',
-    version='0.5',
+    version='0.5.1',
     description='Perspectivew Aware Topic Modeling Toolkit',
     long_description=readme(),
     keywords='topic modeling machine learning',
@@ -18,14 +18,14 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Intended Audience :: Science/Research',
-        ],
+        'Intended Audience :: Science/Research'
+    ],
     # url='https://github.com/boromir674/....',
     author='Konstantinos',
     author_email='k.lampridis@hotmail.com',
     license='GNU GPLv3',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires=['numpy', 'nltk'],
+    install_requires=['numpy', 'nltk', 'gensim', 'tqdm', 'in-place', 'protobuf', 'click'],
     include_package_data=True,
     test_suite='patm.tests',
     zip_safe=False
--- patmtk/transform.py
+++ patmtk/transform.py
@@ -265,4 +265,3 @@ if __name__ == '__main__':
                                    min_df=args.min_df,
                                    apply_zero_index=False)
     # ph.write_cooc_information(args.window, args.min_tf, args.min_df)
-
