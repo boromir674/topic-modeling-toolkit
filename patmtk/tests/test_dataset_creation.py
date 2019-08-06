028373455c32ee1e2b8fc91fb5e51c471f253232
Konstantinos <>
2019-08-06 22:05:49 +0200

    Encapsulate test_dataset in a fixture of session scope to potentially use it in further tests for training, tuning, etc

--- patmtk/tests/test_dataset_creation.py
+++ patmtk/tests/test_dataset_creation.py
@@ -14,9 +14,27 @@ TEST_COLLECTIONS_ROOT_DIR_NAME = 'unittests-collections'
 TEST_COLLECTION = 'unittest-dataset'
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def collections_root_dir(tmpdir):
     return str(tmpdir.mkdir(TEST_COLLECTIONS_ROOT_DIR_NAME))
+
+
+@pytest.fixture(scope='session', params=[[100, 100]])
+def sample_n_real():
+    return request.param
+
+
+@pytest.fixture(scope='session')
+def pipe_n_quantities(sample_n_real):
+    return [TEST_PIPELINE_CFG] + sample_n_real + [1297, 833, 834, 771]
+
+
+@pytest.fixture(scope='session')
+def test_dataset(collections_root_dir, pipe_n_quantities):
+    """A dataset ready to be used for topic modeling training. Depends on the input document sample size to take and resulting actual size"""
+    pipe_handler = PipeHandler(collections_root_dir, 'posts', pipe_n_quantities[1])
+    pipe_handler.pipeline = Pipeline.from_cfg(pipe_n_quantities[0])
+    return pipe_handler.preprocess(TEST_COLLECTION, add_class_labels_to_vocab=True)
 
 
 @pytest.fixture(scope='module')
@@ -24,10 +42,35 @@ def coherence_builder(collections_root_dir):
     return CoherenceFilesBuilder(os.path.join(collections_root_dir, TEST_COLLECTION))
 
 
+
 class DatasetTransformationTester(object):
 
+    #
+    # @pytest.mark.parametrize("pipeline_cfg, sample, nb_docs, nb_bows, unique, vocab_file_len", [
+    #     (TEST_PIPELINE_CFG, 100, 100, 1297, 833, 834)
+    # ])
+    def test_transform(self, pipe_n_quantities, test_dataset, coherence_builder):
+        coherence_builder.create_files(cooc_window=10, min_tf=0, min_df=0, apply_zero_index=False)
+
+        assert test_dataset.name == TEST_COLLECTION
+        assert test_dataset.id == self._id(pipe_n_quantities[2], pipe_n_quantities[0])
+        assert test_dataset._col_len == pipe_n_quantities[2]
+        assert test_dataset.nb_bows == pipe_n_quantities[3]
+        assert test_dataset.unique == pipe_n_quantities[4]
+
+        assert self.file_len(test_dataset.bowf) == pipe_n_quantities[3] + 3
+        assert self.file_len(test_dataset.words) == pipe_n_quantities[5]  # this hack is used because the currently used sample (which takes the first 500 documents) of 500 docs includes only one label
+        assert self.file_len(test_dataset.vowpal) == pipe_n_quantities[2]
+
+        assert self.file_len(self._file('cooc_0_tf.txt')) == pipe_n_quantities[6]
+        assert self.file_len(self._file('cooc_0_df.txt')) == pipe_n_quantities[6]
+        assert self.file_len(self._file('ppmi_0_tf.txt')) == pipe_n_quantities[6]
+        assert self.file_len(self._file('ppmi_0_df.txt')) == pipe_n_quantities[6]
+        assert os.path.isfile(self._file('{}.pkl'.format(test_dataset.id)))
+
     def _formatter(self, pipe_settings):
-        return lambda x: '{}-{}'.format(x, pipe_settings[x] if x in ['lowercase', 'monospace', 'unicode', 'deaccent'] else x)
+        return lambda x: '{}-{}'.format(x, pipe_settings[x] if x in ['lowercase', 'monospace', 'unicode',
+                                                                     'deaccent'] else x)
 
     def _id(self, full_docs, cfg):
         config = ConfigParser.ConfigParser()
@@ -48,28 +91,3 @@ class DatasetTransformationTester(object):
     @staticmethod
     def _file(name):
         return os.path.join(TEST_COLLECTIONS_ROOT_DIR_NAME, TEST_COLLECTION, name)
-
-    @pytest.mark.parametrize("pipeline_cfg, sample, nb_docs, nb_bows, unique, vocab_file_len", [
-        (TEST_PIPELINE_CFG, 100, 100, 1297, 833, 834)
-    ])
-    def test_transform(self, pipeline_cfg, sample, nb_docs, nb_bows, unique, vocab_file_len, collections_root_dir, coherence_builder):
-        pipe_handler = PipeHandler(collections_root_dir, 'posts', sample)
-        pipe_handler.pipeline = Pipeline.from_cfg(pipeline_cfg)
-        test_dataset = pipe_handler.preprocess(TEST_COLLECTION, add_class_labels_to_vocab=True)
-        coherence_builder.create_files(cooc_window=10, min_tf=0, min_df=0, apply_zero_index=False)
-
-        assert test_dataset.name == TEST_COLLECTION
-        assert test_dataset.id == self._id(nb_docs, pipeline_cfg)
-        assert test_dataset._col_len == nb_docs
-        assert test_dataset.nb_bows == nb_bows
-        assert test_dataset.unique == unique
-
-        assert self.file_len(test_dataset.bowf) == nb_bows + 3
-        assert self.file_len(test_dataset.words) == vocab_file_len  # this hack is used because the currently used sample (which takes the first 500 documents) of 500 docs includes only one label
-        assert self.file_len(test_dataset.vowpal) == nb_docs
-
-        assert self.file_len(self._file('cooc_0_tf.txt')) == 771
-        assert self.file_len(self._file('cooc_0_df.txt')) == 771
-        assert self.file_len(self._file('ppmi_0_tf.txt')) == 771
-        assert self.file_len(self._file('ppmi_0_df.txt')) == 771
-        assert os.path.isfile(self._file('{}.pkl'.format(test_dataset.id)))
